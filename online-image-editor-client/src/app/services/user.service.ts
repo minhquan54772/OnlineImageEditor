c3cd47af55f0618e1e596b86c22a15d3165cf8a0
NMQ <ngminhquan.aries@gmail.com>
2024-03-21 21:51:25 +0700

    Client/Server: Add sign up popup + endpoints

--- online-image-editor-client/src/app/services/user.service.ts
+++ online-image-editor-client/src/app/services/user.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { Observable } from 'rxjs';
 import { BaseResponse } from '../payload/response/BaseResponse';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   readonly API_LOGIN: string = '/auth/login';
+  readonly API_SIGN_UP: string = '/users/new';
+  readonly API_FIND_USER_BY_EMAIL = '/users/find';
 
   constructor(private httpService: HttpService) {}
 
@@ -17,5 +20,20 @@ export class UserService {
       password: password,
     };
     return this.httpService.post(this.API_LOGIN, null, loginRequestBody);
+  }
+
+  signup(email: string, password: string): Observable<BaseResponse<User>> {
+    const signupRequestBody = {
+      email: email,
+      password: password,
+    };
+    return this.httpService.post(this.API_SIGN_UP, null, signupRequestBody);
+  }
+
+  findUserByEmail(email?: string): Observable<BaseResponse<User>> {
+    const params = {
+      email: email,
+    };
+    return this.httpService.get(this.API_FIND_USER_BY_EMAIL, params);
   }
 }
--- online-image-editor-server/src/main/java/com/intern/imageEditor/controllers/UserController.java
+++ online-image-editor-server/src/main/java/com/intern/imageEditor/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.intern.imageEditor.controllers;
 
 import com.intern.imageEditor.models.User;
+import com.intern.imageEditor.payload.response.BaseResponse;
 import com.intern.imageEditor.repository.UserRepository;
 import com.intern.imageEditor.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(originPatterns = "http://localhost:4200")
@@ -26,8 +29,15 @@ public class UserController {
     }
 
     @PostMapping("/new")
-    public User createNewUser(@RequestBody User newUser) {
-        return userService.createUser(newUser);
+    public ResponseEntity<BaseResponse<User>> createNewUser(@RequestBody User newUser) {
+        Optional<User> userByEmail = userService.getUserByEmail(newUser.getEmail());
+        if (userByEmail.isPresent()) {
+            // user da ton tai
+            return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(new BaseResponse<>(null, false, "This email is linked to an existed account already"));
+        } else {
+            User user = userService.createUser(newUser);
+            return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(new BaseResponse<>(user, true));
+        }
     }
 
     @PutMapping("/{id}")
@@ -40,6 +50,12 @@ public class UserController {
         userToUpdate.setEmail(user.getEmail());
 
         return ResponseEntity.ok(userRepository.save(userToUpdate));
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<BaseResponse<User>> findUserByEmail(@RequestParam String email) {
+        Optional<User> userByEmail = userService.getUserByEmail(email);
+        return userByEmail.map(user -> ResponseEntity.ok().body(new BaseResponse<>(user, true))).orElseGet(() -> ResponseEntity.status(HttpStatusCode.valueOf(404)).body(new BaseResponse<>(null, false, "This email is not linked to any account")));
     }
 
 
